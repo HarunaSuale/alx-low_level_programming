c2d26d85dc57a007de3c9c30f8373be9dec31eed
Haruna Suale <harunasuale10@gmail.com>
2023-09-12 15:10:38 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-/**
- * main - prints its name, followed by a new line.
- * @argc: argument count
- * @argv: arguments
- *
- * Return: 0
- */
-int main(int argc, char **argv)
-{
-	(void)argc;
 
-	printf("%s\n", argv[0]);
-	return (0);
+/*
+ * main- a program to print 
+ * number of arguments pass to it
+ * Argc and argv concept.
+ * return 0 after compilation.
+ */
+
+int main(int argc, char**argv){
+
+	(void)argc;
+	printf("%s\n", argc-1);
+	return 0;
+
+	
+
 }
+
