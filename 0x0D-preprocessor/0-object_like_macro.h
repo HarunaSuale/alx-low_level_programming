b903cd507102132360546ed70f75bdc65872743f
Haruna Suale <harunasuale10@gmail.com>
2023-11-27 13:40:01 -0800

    done

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef _0_OBJECT_LIKE_MACRO_H_
-#define _0_OBJECT_LIKE_MACRO_H_
+#ifndef _OBJECT_LIKE_MACRO_H_
+#define _OBJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
-#endif /* 0-object_like_macro.h */
+#endif
 
