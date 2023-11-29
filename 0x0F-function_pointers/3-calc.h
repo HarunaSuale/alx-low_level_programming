8b2b5a4cdbf295f581f07c89e19bf09f7cef1dea
Haruna Suale <harunasuale10@gmail.com>
2023-11-29 09:18:29 -0800

    task 3 corrected

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,30 +1,21 @@
-#include "function_pointers.h"
-
+#ifndef _HEADER_
+#define _HEADER_
 /**
-  * int_index - ...
-  * @array: ...
-  * @size: ...
-  * @cmp: ...
-  *
-  * Return: ...
-  */
-int int_index(int *array, int size, int (*cmp)(int))
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
 {
-	int i = 0;
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
-	if (size > 0)
-	{
-		if (array != NULL && cmp != NULL)
-		{
-			while (i < size)
-			{
-				if (cmp(array[i]))
-					return (i);
-
-				i++;
-			}
-		}
-	}
-
-	return (-1);
-}
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+#endif
